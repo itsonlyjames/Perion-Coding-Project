a21d9a9f1d2f2c6293296eb5c4f90f865d9d5532
itsonlyjames <jamesalexandre1995@gmail.com>
2024-02-07 11:27:23 +1100

    Results Page Layout Structure / Data Display

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,7 +5,6 @@ export const actions = {
 	default: async (e) => {
 		const formData = await e.request.formData();
 		let steamID = formData.get('steamID');
-
 		return await retrieveOwnedGames(steamID as string);
 	}
 } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,33 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let form;
+
+	let totalPlayTime: number;
+	let mostPlayedGame: { [key: string]: string };
+
+	type gamesResponse = {
+		playtime_forever: number;
+	};
+
+	onMount(() => {
+		if (!form) return;
+
+		totalPlayTime = form.response.games.reduce(
+			(a: number, b: gamesResponse) => a + b.playtime_forever,
+			0
+		);
+		mostPlayedGame = form.response.games.reduce((a: gamesResponse, b: gamesResponse) =>
+			a.playtime_forever > b.playtime_forever ? a : b
+		);
+	});
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-start p-8">
 	<div class="space-y-5">
-		<form method="POST" class="flex flex-row">
+		<h1 class="h1 text-center">Steam Stats</h1>
+		<form method="POST" class="flex flex-row justify-center">
 			<label for="steamID" class="label">
 				<input
 					name="steamID"
@@ -19,12 +41,47 @@
 
 		{#if form?.response}
 			<h2 class="h2">Results</h2>
+			<div class="grid grid-cols-2 gap-4">
+				{#if mostPlayedGame}
+					<div class="card variant-glass-primary p-6">
+						<h3 class="h3 mb-2">Most Played Game</h3>
+						<h4 class="h4 mb-4">{mostPlayedGame.name}</h4>
+						<p>
+							Played for <b>{mostPlayedGame.playtime_forever}</b> minutes, or
+							<b>{(+mostPlayedGame.playtime_forever / 60).toFixed(2)}</b>
+							hours, or <b>{(+mostPlayedGame.playtime_forever / 1440).toFixed(2)}</b> days
+						</p>
+					</div>
+				{/if}
+				{#if totalPlayTime}
+					<div class="card variant-soft-tertiary p-6">
+						<h4 class="h4 mb-2">Total Time Playing: <b>{totalPlayTime} minutes</b></h4>
+						<h4 class="h4">Total Games Owned: <b>{form.response.game_count}</b></h4>
+					</div>
+				{/if}
+			</div>
 
-			<div class="space-y-5">
-				{#each form?.response.games as game}
-					<div class="card variant-filled-primary p-6">{game.appid}</div>
+			<div class="grid grid-cols-3 gap-8">
+				{#each form.response.games as game}
+					<div class="card variant-filled-secondary flex flex-col">
+						<header class="card-header p-0 rounded-lg">
+							<img
+								src={`https://steamcdn-a.akamaihd.net/steam/apps/${game.appid}/header.jpg`}
+								alt=""
+								class="rounded-t-2xl"
+							/>
+						</header>
+						<section class="p-4 flex flex-col justify-between h-full">
+							<h4 class="h4 mb-3">{game.name}</h4>
+							<p class="align-bottom">
+								Time played: {game.playtime_forever} minutes
+							</p>
+						</section>
+					</div>
 				{/each}
 			</div>
+		{:else}
+			<h3 class="h3">No valid steam ID</h3>
 		{/if}
 	</div>
 </div>
